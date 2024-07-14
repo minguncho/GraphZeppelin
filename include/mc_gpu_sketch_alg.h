3b7cd877a069c8466263717de9503d862be566b4
Evan West <evtwest@yahoo.com>
2024-07-14 17:50:22 -0400

    func decl

--- include/mc_gpu_sketch_alg.h
+++ include/mc_gpu_sketch_alg.h
@@ -78,7 +78,7 @@ private:
   std::vector<CudaStream> streams;
 
   // helper functions for apply_update_batch()
-  size_t get_and_apply_finished_stream(int stream_id, int thr_id);
+  size_t get_and_apply_finished_stream(int thr_id);
   void complete_update_batch(int thr_id, const TaggedUpdateBatch &updates, size_t min_subgraph = 0);
 
   std::mutex sketch_creation_lock;
